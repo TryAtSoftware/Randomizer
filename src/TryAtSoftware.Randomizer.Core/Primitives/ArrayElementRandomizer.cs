572cbffc5acd020abeba656018c92c66fb3fa887
Tony Troeff <tony13troev@gmail.com>
2021-06-10 15:16:50 +0300

    Added some new randomizers.

---
+++ src/TryAtSoftware.Randomizer.Core/Primitives/ArrayElementRandomizer.cs
@@ -0,0 +1,28 @@
+﻿namespace TryAtSoftware.Randomizer.Core.Primitives
+{
+    using System;
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+    using TryAtSoftware.Randomizer.Core.Helpers;
+    using TryAtSoftware.Randomizer.Core.Interfaces;
+
+    public class ArrayElementRandomizer<T> : IRandomizer<T>
+    {
+        [NotNull]
+        private readonly IReadOnlyList<T> _array;
+
+        public ArrayElementRandomizer([NotNull] IReadOnlyList<T> array)
+        {
+            this._array = array ?? throw new ArgumentNullException(nameof(array));
+        }
+
+        public T PrepareRandomValue()
+        {
+            if (this._array.Count == 0)
+                return default;
+
+            var randomNumber =  RandomizationHelper.RandomInteger(0, this._array.Count);
+            return this._array[randomNumber];
+        }
+    }
+}
---
+++ src/TryAtSoftware.Randomizer.Core/Primitives/CollectionRandomizer.cs
@@ -0,0 +1,33 @@
+﻿namespace TryAtSoftware.Randomizer.Core.Primitives
+{
+    using System;
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+    using TryAtSoftware.Randomizer.Core.Helpers;
+    using TryAtSoftware.Randomizer.Core.Interfaces;
+
+    public class CollectionRandomizer<T> : IRandomizer<IEnumerable<T>>
+    {
+        [NotNull]
+        private readonly IRandomizer<T> _singleValueRandomizer;
+
+        public CollectionRandomizer([NotNull] IRandomizer<T> singleValueRandomizer)
+        {
+            this._singleValueRandomizer = singleValueRandomizer ?? throw new ArgumentNullException(nameof(singleValueRandomizer));
+        }
+
+        public IEnumerable<T> PrepareRandomValue()
+        {
+            var randomNumber = RandomizationHelper.RandomInteger(1, 10);
+            var collection = new List<T>(capacity: randomNumber);
+
+            for (var i = 0; i < randomNumber; i++)
+            {
+                var currentRandomValue = this._singleValueRandomizer.PrepareRandomValue();
+                collection.Add(currentRandomValue);
+            }
+
+            return collection;
+        }
+    }
+}
