9e22f0a3bbe7d5e715b808490a6b46270aec0696
Tony Troeff <tony13troev@gmail.com>
2021-06-12 01:35:37 +0300

    Any randomization rule can be instantiated with a Value setter instead of a randomizer.

--- src/TryAtSoftware.Randomizer.Core/RandomizationRule.cs
+++ src/TryAtSoftware.Randomizer.Core/RandomizationRule.cs
@@ -9,21 +9,36 @@
     public class RandomizationRule<TEntity, TValue> : IRandomizationRule<TEntity>
         where TEntity : class
     {
-        public RandomizationRule([NotNull] Expression<Func<TEntity, TValue>> propertySelector, [NotNull] IRandomizer<TValue> randomizer)
+        private readonly IRandomValueSetter<TEntity> _valueSetter;
+        
+        private RandomizationRule(Expression<Func<TEntity, TValue>> propertySelector)
         {
+            if (propertySelector is null)
+                throw new ArgumentNullException(nameof(propertySelector));
+
             var property = propertySelector.GetPropertyInfo();
             this.PropertyName = property.Name;
-            this.Randomizer = randomizer;
+        }
+
+        public RandomizationRule([NotNull] Expression<Func<TEntity, TValue>> propertySelector, [NotNull] IRandomizer<TValue> randomizer)
+            : this(propertySelector)
+        {
+            if (randomizer is null)
+                throw new ArgumentNullException(nameof(randomizer));
+            
+            this._valueSetter = new RandomValueSetter<TEntity, TValue>(this.PropertyName, randomizer, MembersBinderCache<TEntity>.Binder);
+        }
+
+        public RandomizationRule([NotNull] Expression<Func<TEntity, TValue>> propertySelector, [NotNull] IRandomValueSetter<TEntity> valueSetter)
+            : this(propertySelector)
+        {
+            this._valueSetter = valueSetter ?? throw new ArgumentNullException(nameof(valueSetter));
         }
 
         /// <inheritdoc />
         public string PropertyName { get; }
 
         /// <inheritdoc />
-        public IRandomizer<TValue> Randomizer { get; }
-
-        /// <inheritdoc />
-        public IRandomValueSetter<TEntity> GetValueSetter() 
-            => new RandomValueSetter<TEntity,TValue>(this.PropertyName, this.Randomizer, MembersBinderCache<TEntity>.Binder);
+        public IRandomValueSetter<TEntity> GetValueSetter() => this._valueSetter;
     }
 }
