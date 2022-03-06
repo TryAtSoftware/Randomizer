d1a6470d8d9681b214314d7a6825f2339d84f651
Tony Troeff <tony13troev@gmail.com>
2022-03-06 13:35:30 +0200

    Fixed some code smells and wrote more tests.

---
+++ tests/TryAtSoftware.Randomizer.Core.Tests/InstanceBuildingResultTests.cs
@@ -0,0 +1,41 @@
+﻿namespace TryAtSoftware.Randomizer.Core.Tests
+{
+    using System.Linq;
+    using TryAtSoftware.Randomizer.Core.Helpers;
+    using TryAtSoftware.Randomizer.Core.Primitives;
+    using Xunit;
+
+    public class InstanceBuildingResultTests
+    {
+        [Fact]
+        public static void InstanceBuildingResultShouldBeCorrectlyInstantiated()
+        {
+            var randomText = RandomizationHelper.GetRandomString();
+            var instanceBuildingResult = new InstanceBuildingResult<string>(randomText);
+
+            Assert.Equal(randomText, instanceBuildingResult.Instance);
+        }
+
+        [Fact]
+        public static void InstanceBuildingResultShouldBeCorrectlyInstantiatedWithNull()
+        {
+            var instanceBuildingResult = new InstanceBuildingResult<string>(null);
+            Assert.Null(instanceBuildingResult.Instance);
+        }
+
+        [Fact]
+        public static void InstanceBuildingResultShouldCorrectlyDefineTheUsedParametersDuringInstantiation()
+        {
+            var parametersRandomizer = new CollectionRandomizer<string>(new StringRandomizer());
+            var randomParameters = parametersRandomizer.PrepareRandomValue().ToList();
+
+            var randomText = RandomizationHelper.GetRandomString();
+            var instanceBuildingResult = new InstanceBuildingResult<string>(randomText, randomParameters);
+            Assert.Equal(randomText, instanceBuildingResult.Instance);
+            foreach (var randomParameter in randomParameters) Assert.True(instanceBuildingResult.IsUsed(randomParameter));
+
+            var otherRandomParameters = parametersRandomizer.PrepareRandomValue();
+            foreach (var otherRandomParameter in otherRandomParameters) Assert.False(instanceBuildingResult.IsUsed(otherRandomParameter));
+        }
+    }
+}
