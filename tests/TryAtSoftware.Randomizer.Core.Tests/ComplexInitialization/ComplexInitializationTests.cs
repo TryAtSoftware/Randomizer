7d86583a688085ac52a04f70eb09c42bb017bc28
Tony Troeff <tonyslav_troeff@outlook.com>
2023-01-11 20:11:58 +0200

    Increased package versions.

--- tests/TryAtSoftware.Randomizer.Core.Tests/ComplexInitialization/ComplexInitializationTests.cs
+++ tests/TryAtSoftware.Randomizer.Core.Tests/ComplexInitialization/ComplexInitializationTests.cs
@@ -1,46 +1,44 @@
-namespace TryAtSoftware.Randomizer.Core.Tests.ComplexInitialization
+namespace TryAtSoftware.Randomizer.Core.Tests.ComplexInitialization;
+
+using TryAtSoftware.Randomizer.Core.Primitives;
+using TryAtSoftware.Randomizer.Core.Tests.InstanceBuilders;
+using TryAtSoftware.Randomizer.Core.Tests.Models;
+using Xunit;
+
+public class ComplexInitializationTests
 {
-    using TryAtSoftware.Randomizer.Core.Primitives;
-    using TryAtSoftware.Randomizer.Core.Tests.InstanceBuilders;
-    using TryAtSoftware.Randomizer.Core.Tests.Models;
-    using Xunit;
-
-    public class ComplexInitializationTests
+    [Fact]
+    public void ComplexRandomizerShouldWorkCorrectly()
     {
-        [Fact]
-        public void ComplexRandomizerShouldWorkCorrectly()
-        {
-            var instanceBuilder = new PersonInstanceBuilder();
-            var complexRandomizer = new ComplexRandomizer<Person>(instanceBuilder);
-            complexRandomizer.AddRandomizationRule(p => p.Id, new GuidRandomizer());
-            complexRandomizer.AddRandomizationRule(p => p.Name, new StringRandomizer());
-            complexRandomizer.AddRandomizationRule(p => p.Age, new NumberRandomizer());
-            complexRandomizer.AddRandomizationRule(p => p.IsEmployed, new BooleanRandomizer());
-            complexRandomizer.AddRandomizationRule(p => p.EventDate, new DateTimeOffsetRandomizer());
+        var instanceBuilder = new PersonInstanceBuilder();
+        var complexRandomizer = new ComplexRandomizer<Person>(instanceBuilder);
+        complexRandomizer.AddRandomizationRule(p => p.Id, new GuidRandomizer());
+        complexRandomizer.AddRandomizationRule(p => p.Name, new StringRandomizer());
+        complexRandomizer.AddRandomizationRule(p => p.Age, new NumberRandomizer());
+        complexRandomizer.AddRandomizationRule(p => p.IsEmployed, new BooleanRandomizer());
+        complexRandomizer.AddRandomizationRule(p => p.EventDate, new DateTimeOffsetRandomizer());
 
-            var firstPerson = complexRandomizer.PrepareRandomValue();
-            Assert.NotNull(firstPerson);
+        var firstPerson = complexRandomizer.PrepareRandomValue();
+        Assert.NotNull(firstPerson);
 
-            var secondPerson = complexRandomizer.PrepareRandomValue();
-            Assert.NotSame(firstPerson, secondPerson);
-            Assert.NotEqual(firstPerson, secondPerson, new PeopleComparer());
-        }
+        var secondPerson = complexRandomizer.PrepareRandomValue();
+        Assert.NotSame(firstPerson, secondPerson);
+        Assert.NotEqual(firstPerson, secondPerson, new PeopleComparer());
+    }
 
-        [Fact]
-        public void ComplexInstanceBuildingProcessShouldBeExecutedCorrectly()
-        {
-            var instanceBuilder = new GeneralInstanceBuilder<Car>();
-            var complexRandomizer = new ComplexRandomizer<Car>(instanceBuilder);
-            complexRandomizer.AddRandomizationRule(x => x.Make, new StringRandomizer());
-            complexRandomizer.AddRandomizationRule(x => x.Model, new StringRandomizer());
-            complexRandomizer.AddRandomizationRule(x => x.Year, new NumberRandomizer());
+    [Fact]
+    public void ComplexInstanceBuildingProcessShouldBeExecutedCorrectly()
+    {
+        var complexRandomizer = new ComplexRandomizer<Car>();
+        complexRandomizer.AddRandomizationRule(x => x.Make, new StringRandomizer());
+        complexRandomizer.AddRandomizationRule(x => x.Model, new StringRandomizer());
+        complexRandomizer.AddRandomizationRule(x => x.Year, new NumberRandomizer());
 
-            var firstCar = complexRandomizer.PrepareRandomValue();
-            Assert.NotNull(firstCar);
+        var firstCar = complexRandomizer.PrepareRandomValue();
+        Assert.NotNull(firstCar);
 
-            var secondCar = complexRandomizer.PrepareRandomValue();
-            Assert.NotNull(secondCar);
-            Assert.NotSame(firstCar, secondCar);
-        }
+        var secondCar = complexRandomizer.PrepareRandomValue();
+        Assert.NotNull(secondCar);
+        Assert.NotSame(firstCar, secondCar);
     }
 }
