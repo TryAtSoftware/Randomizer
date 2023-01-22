f4ca4a5d30832fa399f8f0b639612abe353ea8ed
Tony Troeff <tonyslav_troeff@outlook.com>
2023-01-23 00:05:12 +0200

    Completed the test validating that randomization rules are executed in the same order as they were initially registered.

--- tests/TryAtSoftware.Randomizer.Core.Tests/ComplexInitialization/ComplexInitializationTests.cs
+++ tests/TryAtSoftware.Randomizer.Core.Tests/ComplexInitialization/ComplexInitializationTests.cs
@@ -50,23 +50,52 @@ public class ComplexInitializationTests
     [Fact]
     public void OrderOfRandomizationRulesShouldBeAccepted()
     {
-        var invocationOrder = new List<int>();
-        var valueSetters = new IRandomValueSetter<Car>[3];
-
-        for (var i = 0; i < 3; i++)
+        var randomizationRuleBuilders = new List<Func<IRandomValueSetter<Person>, IRandomizationRule<Person>>>(capacity: 5)
         {
+            vs => new RandomizationRule<Person, Guid>(x => x.Id, vs),
+            vs => new RandomizationRule<Person, string>(x => x.Name, vs),
+            vs => new RandomizationRule<Person, int>(x => x.Age, vs),
+            vs => new RandomizationRule<Person, DateTimeOffset>(x => x.EventDate, vs),
+            vs => new RandomizationRule<Person, bool>(x => x.IsEmployed, vs)
+        };
+
+        var invocationOrder = new List<int>();
+        var valueSetterMocks = new Mock<IRandomValueSetter<Person>>[randomizationRuleBuilders.Count];
+        var valueSetters = new IRandomValueSetter<Person>[randomizationRuleBuilders.Count];
+
+        for (var i = 0; i < randomizationRuleBuilders.Count; i++)
+        {
+            var randomValueSetterMock = new Mock<IRandomValueSetter<Person>>();
+            
             var valueSetterIndex = i;
-            var randomValueSetterMock = new Mock<IRandomValueSetter<Car>>();
-            randomValueSetterMock.Setup(x => x.SetValue(It.IsAny<Car>())).Callback(() => invocationOrder.Add(valueSetterIndex));
+            randomValueSetterMock.Setup(x => x.SetValue(It.IsAny<Person>())).Callback(() => invocationOrder.Add(valueSetterIndex));
+
+            valueSetterMocks[i] = randomValueSetterMock;
             valueSetters[i] = randomValueSetterMock.Object;
         }
 
-        var repetitionCount = 5;
-        for (var i = 0; i < repetitionCount; i++)
+        for (var i = 0; i < 10; i++)
         {
-            var complexRandomizer = new ComplexRandomizer<Car>();
-            var valueSetterIndices = Enumerable.Range(0, 3);
-                invocationOrder.Clear();
+            var complexRandomizer = new ComplexRandomizer<Person>();
+            var valueSetterIndices = Enumerable.Range(0, randomizationRuleBuilders.Count).ToArray();
+            TestsHelper.Shuffle(valueSetterIndices);
+
+            for (var j = 0; j < randomizationRuleBuilders.Count; j++)
+            {
+                var rule = randomizationRuleBuilders[j].Invoke(valueSetters[valueSetterIndices[j]]);
+                complexRandomizer.AddRandomizationRule(rule);
+            }
+
+            var person = complexRandomizer.PrepareRandomValue();
+            Assert.NotNull(person);
+            
+            Assert.Equal(randomizationRuleBuilders.Count, invocationOrder.Count);
+            for (var j = 0; j < randomizationRuleBuilders.Count; j++) Assert.Equal(valueSetterIndices[j], invocationOrder[j]);
+            for (var j = 0; j < randomizationRuleBuilders.Count; j++) valueSetterMocks[j].Verify(x => x.SetValue(person));
+            
+            invocationOrder.Clear();
         }
+        
+        for (var i = 0; i < randomizationRuleBuilders.Count; i++) valueSetterMocks[i].VerifyNoOtherCalls();
     }
 }
--- tests/TryAtSoftware.Randomizer.Core.Tests/TestsHelper.cs
+++ tests/TryAtSoftware.Randomizer.Core.Tests/TestsHelper.cs
@@ -1,6 +1,7 @@
 namespace TryAtSoftware.Randomizer.Core.Tests;
 
 using System.Collections.Generic;
+using TryAtSoftware.Randomizer.Core.Helpers;
 
 public static class TestsHelper
 {
@@ -13,5 +14,16 @@ public static class TestsHelper
         yield return new object[] { "\r" };
         yield return new object[] { "\n" };
         yield return new object[] { "\t\r\n" };
+    }
+
+    public static void Shuffle<T>(T[] array, int iterations = 20)
+    {
+        for (var i = 0; i < iterations; i++)
+        {
+            var a = RandomizationHelper.RandomInteger(0, array.Length);
+            var b = RandomizationHelper.RandomInteger(0, array.Length);
+
+            (array[a], array[b]) = (array[b], array[a]);
+        }
     }
 }
