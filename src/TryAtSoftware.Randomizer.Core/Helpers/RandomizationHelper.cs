36b587d4bc27d8c89e00fd879c359e8c6715cf3a
Tony Troeff <tonyslav_troeff@outlook.com>
2023-08-11 00:51:37 +0300

    Improving the randomization logic so that it supports 32 and 64 bit types

--- src/TryAtSoftware.Randomizer.Core/Helpers/RandomizationHelper.cs
+++ src/TryAtSoftware.Randomizer.Core/Helpers/RandomizationHelper.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 
 public static class RandomizationHelper
@@ -11,28 +10,47 @@ public static class RandomizationHelper
     public const string LOWER_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
     public const string UPPER_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public const string DIGITS = "0123456789";
+    public const string ALL_CHARACTERS = $"{LOWER_CASE_LETTERS}{UPPER_CASE_LETTERS}{DIGITS}";
+
+    private static Random _random = new ();
+
+    public static int RandomInteger() => RandomInteger(0, int.MaxValue);
 
     public static int RandomInteger(int inclusiveBottomBound, int exclusiveUpperBound)
     {
         if (exclusiveUpperBound <= inclusiveBottomBound) throw new InvalidOperationException("The maximum value for the random number that should be generated cannot be lower than or equal to the minimum.");
-        if (exclusiveUpperBound == inclusiveBottomBound + 1) return inclusiveBottomBound;
 
-        using var rng = new RNGCryptoServiceProvider();
-        var data = new byte[4];
-        rng.GetBytes(data);
+        var range = (uint)(exclusiveUpperBound - inclusiveBottomBound);
+        var limit = uint.MaxValue - uint.MaxValue % range;
 
-        var generatedValue = BitConverter.ToInt32(data, startIndex: 0);
+        uint result;
+        do
+        {
+            result = RandomUInt32();
+        } while (result > limit);
 
-        var difference = exclusiveUpperBound - inclusiveBottomBound;
-        var differenceOffset = Math.Abs(generatedValue) % difference;
-        return inclusiveBottomBound + differenceOffset;
+        return inclusiveBottomBound + (int)(result % range);
     }
 
-    public static string GetRandomString()
+    public static long RandomLongInteger() => RandomLongInteger(0, long.MaxValue);
+
+    public static long RandomLongInteger(long inclusiveBottomBound, long exclusiveUpperBound)
     {
-        var charactersMask = $"{LOWER_CASE_LETTERS}{UPPER_CASE_LETTERS}{DIGITS}";
-        return GetRandomString(RandomInteger(30, 80), charactersMask);
+        if (exclusiveUpperBound <= inclusiveBottomBound) throw new InvalidOperationException("The maximum value for the random number that should be generated cannot be lower than or equal to the minimum.");
+
+        var range = (ulong)(exclusiveUpperBound - inclusiveBottomBound);
+        var limit = ulong.MaxValue - ulong.MaxValue % range;
+
+        ulong result;
+        do
+        {
+            result = RandomUInt64();
+        } while (result > limit);
+
+        return inclusiveBottomBound + (long)(result % range);
     }
+
+    public static string GetRandomString() => GetRandomString(RandomInteger(30, 80), ALL_CHARACTERS);
 
     public static string GetRandomString(int length, string charactersMask)
     {
@@ -64,12 +82,22 @@ public static class RandomizationHelper
 
     public static DateTimeOffset GetRandomDateTimeOffset(bool historical = false)
     {
-        var randomRepetitionsCount = RandomInteger(3, 6);
-        long ticks = 1;
-        for (var i = 0; i < randomRepetitionsCount; i++)
-            ticks *= RandomInteger(10, 1000);
-
+        var ticks = RandomLongInteger(0, long.MaxValue);
         var randTimeSpan = new TimeSpan(ticks);
         return historical ? DateTimeOffset.Now.Add(-randTimeSpan) : DateTimeOffset.Now.Add(randTimeSpan);
+    }
+
+    private static uint RandomUInt32()
+    {
+        var buffer = new byte[4];
+        _random.NextBytes(buffer);
+        return BitConverter.ToUInt32(buffer);
+    }
+
+    private static ulong RandomUInt64()
+    {
+        var buffer = new byte[8];
+        _random.NextBytes(buffer);
+        return BitConverter.ToUInt64(buffer);
     }
 }
